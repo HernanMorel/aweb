d4b8ec56b04b7caf6db96208945255495f26ee00
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2023-06-01 23:08:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1>Shanghai City</h1>
+          <h1>I'm pretending to be Aposta.la</h1>
         </div>
+        <a
+          href="https://pr-1176.d1gfrv5ovpw117.amplifyapp.com/apostala?ci=1234567"
+          referrerPolicy="origin"
+        >
+          <button>
+            Click Me Carlos, Rodney, Vale, and last but not least... Abel
+          </button>
+        </a>
       </header>
     </div>
   );
